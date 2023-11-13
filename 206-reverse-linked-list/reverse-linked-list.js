4886eb074ca6fcf679689b296b843a7d1368936f
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-11-13 18:40:31 +0530

    Time: 62 ms (17.17%) | Memory: 44.7 MB (8.81%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    //take three pointers, assign current=head
+    var prev=null;
+    var current=head;
+    var next=null;
+    //simple reversal algorithm
+    while(current!=null){
+        next=current.next;
+        current.next=prev;
+        prev=current;
+        current=next;
+    }
+    //assign head to prev pointer
+    head=prev;
+    return head;
+};
