f47eaa8cfb8115bae79325b34154f7c1318654c2
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-11-16 20:54:55 +0530

    Time: 74 ms (5.66%) | Memory: 46 MB (9.94%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -16,5 +16,25 @@ var maxDepth = function(root) {
     }
     //recursive formulae to find maximum depth
     // 1+max(dfs(left),dfs(right))
-    return (1+Math.max(maxDepth(root.left),maxDepth(root.right)));
+    //return (1+Math.max(maxDepth(root.left),maxDepth(root.right)));
+
+    //the below code uses iterative dfs approach
+    //push the root into stack
+    let stack = [[root,1]];
+    let res=0;
+    //traverse through stack
+    while(stack.length){
+        //pop the last node
+       let [node,depth] = stack.pop();
+       //if node is not null
+       if(node){
+           //store the maximum depth
+           res=Math.max(res,depth);
+           //if right/left node exists , push them into stack with 
+           //depth+1 
+           if(node.left)stack.push([node.left,depth+1]);
+           if(node.right)stack.push([node.right,depth+1]);
+       }
+    }
+    return res;
 };
