9fac35ef249e4e377f232038ccb3eb2ba44b0113
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-11-22 21:41:51 +0530

    Time: 68 ms (55.19%) | Memory: 44.1 MB (99.12%) - LeetSync

---
+++ 39-combination-sum/combination-sum.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    let res=[];
+    const dfs = (i,curr,total)=>{
+        if(total==target){
+            //make a copy of curr array,then push into res array
+            return res.push([...curr]);
+        }
+        if(i>=candidates.length || total>=target){
+            //base case 2
+            return;
+        }
+        //backtracking solution
+        //add current candidate to curr array
+        curr.push(candidates[i]);
+        //call dfs with curr value
+        dfs(i,curr,total+candidates[i]);
+        //now pop curr value
+        curr.pop();
+        //go to next index and call dfs without using curr value
+        dfs(i+1,curr,total);
+    };
+    //initialize dfs
+    dfs(0,[],0);
+    return res;
+};
