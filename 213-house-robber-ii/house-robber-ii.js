5e0e43d9f86e4f208e50db175edf2c9a4d8bf376
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-11-28 17:20:19 +0530

    Time: 44 ms (87.56%) | Memory: 42.3 MB (19.62%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.js
+++ 213-house-robber-ii/house-robber-ii.js
@@ -2,19 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums) {
-    if(nums.length==1){
-        //if only one house, then return that house
-        return nums[0];
-    }
-    /**
+/**
     As the last house is connected back to first house in a circular way,
     we cannot have a loot which involves both first house and last house.
     So, we can split the problem into 2 parts:
         1. To start from house1 and go till house n-1 (last house - 1)
         2. To start from house2 and go till house n (last house)
         and then take the maximum of the two loots and return.
-    */
+*/
+var rob = function(nums) {
+    if(nums.length==1){
+        //if only one house, then return that house
+        return nums[0];
+    }
     //nums.slice(1) -> returns an array without first element of array
     //nums.slice(0,-1) -> returns an array without last element of array
     return Math.max(houseRobber1(nums.slice(1)),houseRobber1(nums.slice(0,-1)));
