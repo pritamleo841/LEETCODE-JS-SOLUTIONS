262b9677a022754996b2d2890e58aa2a14c5a286
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-01 18:08:21 +0530

    Time: 67 ms (21.20%) | Memory: 43.3 MB (79.63%) - LeetSync

--- 139-word-break/word-break.js
+++ 139-word-break/word-break.js
@@ -16,19 +16,49 @@ Check for Segmentation: For each j in the range, we check if dp[j] is True and i
 
 Result: Finally, we return dp[n], which will be True if the entire string can be segmented into words from the dictionary.
 */
-var wordBreak = function(s, wordDict) {
-    let n = s.length;
-    let dp = new Array(n+1).fill(false);
-    dp[0]=true;
-    let maxLen = Math.max(...wordDict.map(word=>word.length));
+// var wordBreak = function(s, wordDict) {
+//     let n = s.length;
+//     let dp = new Array(n+1).fill(false);
+//     //Base case 1- we can make an empty string, so 0 would be true
+//     dp[0]=true;
+//     let maxLen = Math.max(...wordDict.map(word=>word.length));
 
-    for(let i=1;i<=n;i++){
-        for(let j=i-1;j>=Math.max(i-maxLen-1,0);j--){
-            if(dp[j] && wordDict.includes(s.substring(j,i))){
-                dp[i]=true;
-                break;
+//     for(let i=1;i<=n;i++){
+//         for(let j=i-1;j>=Math.max(i-maxLen-1,0);j--){
+//             if(dp[j] && wordDict.includes(s.substring(j,i))){
+//                 dp[i]=true;
+//                 break;
+//             }
+//         }
+//     }
+//     return dp[n];
+// };
+const wordBreak = (s, wordDict) => {
+    // Tablulation
+    // Init our table, this will be boolean values since return is boolean
+    const table = Array(s.length + 1).fill(false);
+    // Seed our simple case, this would be base case in recursive solution
+    // Basically, we can make an empty string, so 0 would be true
+    // In our table, each index refers to the letter before
+    // So table[0] is "", table[1] is "l", table[2] is "e", if testcase is "leetcode"
+    table[0] = true;
+    
+    // Loop table
+    for(let i = 0; i < table.length; i++) {
+        // We only want to process if current table position is true
+        if(!table[i]) continue;
+        
+        // Loop all letters in wordDict
+        for(let w of wordDict) {
+            // We want to test if word matches where we are in s, and the word
+            // We can slice from i to i + w.length and compare with the word
+            if(s.slice(i, i + w.length) === w) {
+                // If we pass the condition above, and the table index we are trying to update is in bounds, update to true
+                if(i + w.length <= table.length) table[i + w.length] = true;
             }
         }
     }
-    return dp[n];
+    
+    // The last item in our table can be returned, as it will be updated to true if we found a solution
+    return table[s.length];
 };
