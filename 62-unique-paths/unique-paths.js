f5431c8b8413d6b243ba86324570b88104e4308e
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-02 19:33:55 +0530

    Time: 45 ms (90.63%) | Memory: 41.7 MB (86.81%) - LeetSync

---
+++ 62-unique-paths/unique-paths.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    //DP - Tabulation Problem
+    let dp = new Array(m).fill(0).map(()=>new Array(n).fill(0));
+    //Base cases : If we are in last row, i == m-1,
+    //we only have the choice to move RIGHT,Hence number of moves will be 1.
+    //If we are in last column, j == n-1,
+    //we only have the choice to move DOWN,Hence number of moves will be 1.
+    for(let i=0;i<m;i++){
+        dp[i][0]=1;
+    }
+    for(let i=0;i<n;i++){
+        dp[0][i]=1;
+    }
+    //A cell (i,j) can be reached either from (i−1,j) or (i,j−1),
+    //and thus the number of unique paths to (i,j)
+    //is the sum of the number of unique paths to these two cel
+    for(let i=1;i<m;i++){
+        for(let j=1;j<n;j++){
+            dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        }
+    }
+    
+    return dp[m-1][n-1];
+};
