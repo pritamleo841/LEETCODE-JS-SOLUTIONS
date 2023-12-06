e5898633c9c117384c7af25fe8a70b9049f2dec3
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-06 20:25:37 +0530

    Time: 56 ms (71.87%) | Memory: 43.9 MB (81.76%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -4,12 +4,18 @@
  */
 var missingNumber = function(nums) {
     //return nums.reduce((a,c,i)=>a+i+1-c,0);
-    
+
+    //Init the accumulator to nums.length
+    //Then XOR (^) the current number with the index
+    //Then XOR equals (^=) with the accumulator
+    //(0^1^3)^(0^1^2^3)=>2
     let missingNumber = nums.length;
     for(let i=0;i<nums.length;i++) {
         const xor=(i^nums[i]);
         missingNumber^= xor;
     }
     return missingNumber;
+    //return nums.reduce((acc, cur, i)=>acc^=i^cur,nums.length);
+
     
 };
