aa327e8f6c8d2ab2a53b5bf3562909528b70bf2d
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-07 17:32:08 +0530

    Time: 60 ms (6.50%) | Memory: 41.5 MB (67.07%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.js
+++ 371-sum-of-two-integers/sum-of-two-integers.js
@@ -7,9 +7,9 @@ var getSum = function(a, b) {
     //half added implementation
     //when b=0 , it means there is no carry and we can return a
     while(b!=0){
-        let carry=(a&b);
-        a=a^b;
-        b=carry<<1;
+        let carry=(a&b);    //carry
+        a=a^b;  //sum without carry
+        b=carry<<1; //shift carry by 1
     }
     return a;
 };
