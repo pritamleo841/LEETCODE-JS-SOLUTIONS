e06c1632bed00207833988991df4ae1352005a8a
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-12 21:57:17 +0530

    Time: 174 ms (88.34%) | Memory: 75.6 MB (62.33%) - LeetSync

--- 883-car-fleet/car-fleet.js
+++ 883-car-fleet/car-fleet.js
@@ -5,6 +5,7 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
+    //O(nlogn)
     let count=0;
     let map = new Map();
     let lastTime = -1; //the time the last car reached target
@@ -25,4 +26,21 @@ var carFleet = function(target, position, speed) {
         }
     }
     return count;
+
+    /**
+    Without Sorting : O(n)
+    const arr = new Array(target).fill(0);
+    for (let i = 0; i < position.length; i++) {
+        arr[position[i]] = (target - position[i]) / speed[i];
+    }
+    let fleets = 0, prev = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] <= prev) continue;
+        fleets++;
+        prev = arr[i];
+    }
+    return fleets;
+    
+    
+     */
 };
