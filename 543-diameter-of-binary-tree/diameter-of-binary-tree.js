b53d9c130097db34a4658f8b405d0adba1815d3b
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-19 20:13:11 +0530

    Time: 65 ms (36.76%) | Memory: 46 MB (32.30%) - LeetSync

---
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let diameter=0;
+    const dfs = (root)=>{
+        //if root is null, means 0 path
+        if(!root){
+            return 0;
+        }
+        //dfs for each subtree
+        let left=dfs(root.left);
+        let right=dfs(root.right);
+        //Get higher between current max diameter or 
+        //the heights of the left and right subtrees
+        diameter=Math.max(diameter,left+right);
+        //return height of current node by taking max of left 
+        //or right and adding 1 to account for current node
+        return (1+Math.max(left,right));
+    }
+    dfs(root);
+    return diameter;
+};
