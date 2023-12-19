1c2f213d366b84037c8b502f370907d18b772639
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2023-12-19 19:54:30 +0530

    Time: 74 ms (22.42%) | Memory: 45 MB (59.72%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.js
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.js
@@ -13,14 +13,24 @@
 var reverseKGroup = function(head, k) {
     let dummy = new ListNode(0);
     dummy.next=head;
+    //prevGroupTail pointer to the dummy node
     let prevGroupTail = dummy;
 
     while(head){
+        /**
+        Set groupStart to the current head, 
+        and find groupEnd using the getGroupEnd function
+        */
         let groupStart=head;
         let groupEnd=getGroupEnd(head,k);
         if(!groupEnd){
             break;
         }
+        //Set nextGroupStart to groupEnd->next,
+        //and then set groupEnd->next to null 
+        //to separate the group to be reversed from the rest of the list.
+        //Set prevGroupTail->next to the new head of the reversed group.
+        //Then set groupStart->next to nextGroupStart
         prevGroupTail.next=reverseList(groupStart,groupEnd.next);
         prevGroupTail=groupStart;
         head=prevGroupTail.next;
