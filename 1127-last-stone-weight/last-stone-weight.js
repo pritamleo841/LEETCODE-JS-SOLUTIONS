2c2991d2dedb8f0d8eed973256b4ce236d8c0b5d
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-02 19:35:52 +0530

    Time: 107 ms (5.02%) | Memory: 43.5 MB (37.45%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.js
+++ 1127-last-stone-weight/last-stone-weight.js
@@ -3,19 +3,29 @@
  * @return {number}
  */
 var lastStoneWeight = function(stones) {
-    //use max priority queue for picking max stones
-    let pq = new MaxPriorityQueue();
-    for(let s of stones){
-        pq.enqueue(s);
+    // //use max priority queue for picking max stones
+    // let pq = new MaxPriorityQueue();
+    // for(let s of stones){
+    //     pq.enqueue(s);
+    // }
+    // //simulation
+    // while(pq.size()>1){
+    //     let first = pq.dequeue().element;
+    //     let second = pq.dequeue().element;
+    //     if(first!=second){
+    //         pq.enqueue(first-second);
+    //     }
+    // }
+    // //return remaining stone/0 if no stone left
+    // return pq.size()==0?0:pq.front().element;
+
+    //using sorting algorithm
+    if(stones.length<2){
+        return stones;
     }
-    //simulation
-    while(pq.size()>1){
-        let first = pq.dequeue().element;
-        let second = pq.dequeue().element;
-        if(first!=second){
-            pq.enqueue(first-second);
-        }
-    }
-    //return remaining stone/0 if no stone left
-    return pq.size()==0?0:pq.front().element;
+    stones.sort((a,b)=>a-b);
+    let first = stones.pop();
+    let second = stones.pop();
+    stones.push(Math.abs(first-second));
+    return lastStoneWeight(stones);
 };
