0e4b2ddba5b0c9def57022d7627b1eca8158dc1c
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-02 20:01:49 +0530

    Time: 377 ms (12.87%) | Memory: 62.9 MB (37.00%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -5,5 +5,18 @@
  */
 var kClosest = function(points, k) {
     //we can use sorting to find k closest points
-    return points.sort((a,b)=>(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1])).slice(0,k);
+    // return points.sort((a,b)=>(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1])).slice(0,k);
+
+    //Other better solution is to use min heap
+    const res=[];
+    const minHeap = new MinPriorityQueue();
+    points.forEach((p)=>{
+        //no need to find the sqrt here
+        const dist = (p[0]*p[0]+p[1]*p[1]);
+        minHeap.enqueue(p,dist);
+    });
+    while(res.length<k){
+        res.push(minHeap.dequeue().element);
+    }
+    return res;
 };
