759bd84376f157fb308be864b3ee3d3ee7e28e19
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-03 22:53:59 +0530

    Time: 180 ms (37.71%) | Memory: 70.6 MB (15.10%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -5,8 +5,19 @@
  */
 var findKthLargest = function(nums, k) {
     //with sorting : O(nlogn)
-    nums.sort((a,b)=>a-b);
-    return nums[nums.length-k];
-    //with max heap : O(n+klogn), slightly better solution
+    // nums.sort((a,b)=>a-b);
+    // return nums[nums.length-k];
+    //with min heap : O(n+klogn), slightly better solution
+    const q = new MinPriorityQueue();
+    for (let i = 0; i < k; i++) {
+        q.enqueue(nums[i]);
+    }
+    for (let i = k; i < nums.length; i++) {
+        if (nums[i] > q.front().element) {
+            q.dequeue();
+            q.enqueue(nums[i]);
+        }
+    }
+    return q.front().element;
     //With quick select : O(n) in average case
 };
