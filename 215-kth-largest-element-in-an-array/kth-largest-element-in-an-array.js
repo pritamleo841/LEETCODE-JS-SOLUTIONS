c6adb464e993ed30ba575126b47dba137750509e
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-03 23:09:49 +0530

    Time: 174 ms (38.67%) | Memory: 72.1 MB (9.15%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -4,10 +4,11 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    //with sorting : O(nlogn)
+    //With sorting : O(nlogn)
     // nums.sort((a,b)=>a-b);
     // return nums[nums.length-k];
-    //with min heap : O(n+klogn), slightly better solution
+
+    //With min heap : O(n+klogn), slightly better solution
     const q = new MinPriorityQueue();
     for (let i = 0; i < k; i++) {
         q.enqueue(nums[i]);
@@ -19,5 +20,26 @@ var findKthLargest = function(nums, k) {
         }
     }
     return q.front().element;
+
     //With quick select : O(n) in average case
+    // if (!nums.length) return nums;
+    // // standard quick select algorithm
+    // let random = (i, j) => Math.trunc(i + Math.random()*(j - i));
+    // let quick_select = (_nums, k) => {
+    //     if (!_nums.length) return false;
+    //     let pivot = random(0, _nums.length);
+    //     // divide
+    //     let [left, right] = [[], []];
+    //     for (let [i, e] of _nums.entries()) {
+    //         if (i == pivot) continue;
+    //         if (e < _nums[pivot]) left.push(e);
+    //         else right.push(e);
+    //     }
+    //     // conquer
+    //     if (left.length == k) return _nums[pivot];
+    //     if (left.length > k) return quick_select(left, k);
+    //     return quick_select(right, k - (left.length+1));
+    // }
+    // return quick_select(nums, nums.length - k);
+
 };
