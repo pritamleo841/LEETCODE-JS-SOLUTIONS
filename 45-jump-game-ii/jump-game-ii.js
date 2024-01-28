5fa3ec5c092a8b70a5887624f321eb695aa97123
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-28 13:56:34 +0530

    Time: 54 ms (76.55%) | Memory: 50.9 MB (13.02%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    //greedy way- simple bfs algorithm
+    /**
+    e.g.,[2,3,1,1,4]=>
+    [[2],[3,1],[1,4]] where 4 is the target. 
+    So to reach from 2 to 4 we need 2 jumps;
+    */
+    let jumps=0;
+    let l=0,r=0;
+
+    while(r<nums.length-1){
+        let farthest=0;
+        for(let i=l;i<=r;i++){
+            //current jump = i+nums[i]
+            farthest=Math.max(farthest,i+nums[i]);
+        }
+        l=r+1;
+        r=farthest;
+        jumps++;
+    }
+    return jumps;
+};
