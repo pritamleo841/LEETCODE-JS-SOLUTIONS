080692d06231a283d8180cb645a283846c921688
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-01-28 13:57:06 +0530

    Time: 53 ms (81.28%) | Memory: 50.2 MB (16.35%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -11,13 +11,13 @@ var jump = function(nums) {
     */
     let jumps=0;
     let l=0,r=0;
-
     while(r<nums.length-1){
         let farthest=0;
         for(let i=l;i<=r;i++){
             //current jump = i+nums[i]
             farthest=Math.max(farthest,i+nums[i]);
         }
+        //update left and right pointers
         l=r+1;
         r=farthest;
         jumps++;
