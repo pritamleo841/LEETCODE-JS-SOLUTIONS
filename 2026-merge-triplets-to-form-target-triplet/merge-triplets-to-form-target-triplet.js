67df2220f32bfdf5900a31e82c827042915ab92a
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-01 00:30:12 +0530

    Time: 139 ms (53.45%) | Memory: 79.5 MB (44.25%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.js
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.js
@@ -6,8 +6,9 @@
 var mergeTriplets = function(triplets, target) {
    const set = new Set();
     for(let triplet of triplets){
+        //check for bad triplets
         if(triplet[0] > target[0] || triplet[1] > target[1] || triplet[2] > target[2]) continue;
-
+        //save good triplets
         for(let i=0; i< triplet.length; i++){
             if(triplet[i] === target[i]) set.add(i);
         }
