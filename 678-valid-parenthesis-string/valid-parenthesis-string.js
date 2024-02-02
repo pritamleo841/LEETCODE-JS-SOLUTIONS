a4fb75b214f492a3c96b30c7d8ca038833b5f546
Pritam Sharma <72207683+pritamleo841@users.noreply.github.com>
2024-02-02 09:02:42 +0530

    Time: 48 ms (80.18%) | Memory: 48.5 MB (24.89%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/valid-parenthesis-string.js
@@ -2,30 +2,31 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkValidString = function (s) {
-    var leftMin = 0;
-    var leftMax = 0;
-
-    for (var c of s) {
-        if (c === '(') {
+var checkValidString = function(s) {
+    let leftMin=0,leftMax=0;
+    // open parentheses count in range [cmin, cmax]
+    for(let c of s){
+        if(c==='('){
             leftMin++;
             leftMax++;
-        } else if (c === ')') {
+        }
+        else if(c===')'){
             leftMin--;
             leftMax--;
-        } else {
+        }
+        else{
+            // if `*` become `(` then openCount++
+            // if `*` become `)` then openCount--
             leftMin--;
             leftMax++;
+            // if `*` become `` then nothing happens
+            // So openCount will be in new range [cmin-1, cmax+1]
         }
-
-        if (leftMax < 0) {
-            return false;
-        }
-
-        if (leftMin < 0) {
-            leftMin = 0;
-        }
+        // Currently, don't have enough open parentheses to match close parentheses-> Invalid
+        if(leftMax<0)return false;
+        //// It's invalid if open parentheses count < 0 that's why cmin can't be negative
+        if(leftMin<0)leftMin=0;
     }
-
-    return leftMin === 0;
+    // Return true if can found `openCount == 0` in range [cmin, cmax]
+    return leftMin===0;
 };
