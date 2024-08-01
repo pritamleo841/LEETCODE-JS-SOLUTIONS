3abab3e1122d3a07580acb49a953b5ad3f93a2b7
Pritam Sharma <sharmapritam841@gmail.com>
2024-08-01 20:32:27 +0530

    Time: 52 ms (79.35%), Space: 51.8 MB (71.86%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+    let [left,right]=[0,nums.length-1];
+    while(left<right){
+        let mid = left+Math.floor((right-left+1)/2);
+        if(target<nums[mid]){
+            right=mid-1;
+        }
+        else
+            left=mid;
+    }
+    return nums[left]===target?left:-1;
+};
