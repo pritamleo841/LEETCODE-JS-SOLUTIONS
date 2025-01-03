4090936faaa31a78deec11d992244f56706629a7
Pritam Sharma <sharmapritam841@gmail.com>
2025-01-03 19:57:13 +0530

    Time: 51 ms (6.67%), Space: 71.5 MB (12.59%) - LeetHub

---
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var waysToSplitArray = function(nums) {
+    let prefix = Array(nums.length).fill(0);
+    let postfix = Array(nums.length).fill(0);
+
+    for(let i=0;i<nums.length;i++){
+        if(i==0)
+            prefix[i]=nums[i];
+        else
+            prefix[i]=prefix[i-1]+nums[i];
+    }
+
+    for(let i=nums.length-1;i>=0;i--){
+        if(i==nums.length-1)
+            postfix[i]=nums[i];
+        else
+            postfix[i]=postfix[i+1]+nums[i];
+    }
+
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        if(i!=nums.length-1 && prefix[i]>=postfix[i+1])count++;
+    }
+
+    console.log(prefix,postfix,count);
+    return count;
+};
