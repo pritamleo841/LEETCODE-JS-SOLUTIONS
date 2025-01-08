e54b85d6420502e3ca56d1ee0ef39d7487be8937
Pritam Sharma <sharmapritam841@gmail.com>
2025-01-08 19:39:09 +0530

    Time: 2 ms (82.98%), Space: 50.8 MB (62.77%) - LeetHub

---
+++ 3309-count-prefix-and-suffix-pairs-i/3309-count-prefix-and-suffix-pairs-i.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var countPrefixSuffixPairs = function(words) {
+
+    const isPrefixAndSuffix = (str1,str2) =>{
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    };
+
+    let count=0;
+    for(let i=0;i<words.length;i++){
+        for(let j=i+1;j<words.length;j++){
+            if(isPrefixAndSuffix(words[i],words[j])){
+                count++;
+            }
+        }
+    }
+    return count;
+};
