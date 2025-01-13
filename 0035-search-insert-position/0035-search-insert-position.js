45a429eaef6fa41f1ca8176e09987e987997676b
Pritam Sharma <sharmapritam841@gmail.com>
2025-01-13 21:55:04 +0530

    Time: 0 ms (100%), Space: 49.9 MB (10.25%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -7,7 +7,7 @@ var searchInsert = function(nums, target) {
     let [left,right]=[0,nums.length];
     while(left<right){
         let mid = left+Math.floor((right-left)/2);
-        if(nums[mid]>=target){
+        if(nums[mid]>=target){ //find lowerbound(arr,target)
             right=mid;
         }else{
             left=mid+1;
